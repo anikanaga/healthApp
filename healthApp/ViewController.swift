aa8e9cc931cda00a5bfebe74152c23060e1f6298
Anika Naga <anunaga77@gmail.com>
2020-06-09 13:21:56 -0400

    Initial Commit

---
+++ healthApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  healthApp
+//
+//  Created by Anika Naga on 6/9/20.
+//  Copyright © 2020 Anika Naga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
