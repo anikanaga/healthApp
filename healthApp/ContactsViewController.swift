7fef00bbd0d14e2180f22b48cb207ff625232fc6
Anika Naga <anunaga77@gmail.com>
2020-06-10 10:49:33 -0400

    input phone numbers

---
+++ healthApp/ContactsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ContactsViewController.swift
+//  healthApp
+//
+//  Created by Anika Naga on 6/10/20.
+//  Copyright © 2020 Anika Naga. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func callMe(_ sender: Any) {
+        
+    if let phoneURL = NSURL(string: ("tel://" + "4044226144")) {
+
+            let alert = UIAlertController(title: ("Call " + "4044226144" + "?"), message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Call", style: .default, handler: { (action) in
+                UIApplication.shared.open(phoneURL as URL, options: [:], completionHandler: nil)
+            }))
+
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ healthApp/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  healthApp
+//
+//  Created by Anika Naga on 6/10/20.
+//  Copyright © 2020 Anika Naga. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
